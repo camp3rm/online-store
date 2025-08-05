59ccd76891e4ba5fd59fd1b9c2daa3480c6f481d
Olek <olejkocamp@gmail.com>
2025-08-05 20:35:43 +0200

    fixed layout and page files

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import data from '../public/images/decor_plants.json';
-import Header from '@components/header/Header';
 
 export default async function Home() {
 	return (
-		<div>
-			<Header />
+		<div className="main_page">
+			<p>Hello</p>
 		</div>
 	);
 }

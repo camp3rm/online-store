92da3cd0dc389fd54d69ec33305ff07b834ef7e2
Olek <olejkocamp@gmail.com>
2025-08-08 19:32:24 +0200

    fixed header UI

--- app/components/header/Header.tsx
+++ app/components/header/Header.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React, { use, useState } from 'react';
+import React, { useState } from 'react';
 import Link from 'next/link';
 import './header.scss';
 import logo from '@assets/Logo.svg';
